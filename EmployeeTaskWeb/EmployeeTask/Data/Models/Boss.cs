42233899ea36f9d359dab81580d782d660beaf15
admin <stefanivanovc@gmail.com>
2023-03-12 10:42:29 +0200

    Add boss and team models in data, add migration

---
+++ EmployeeTaskWeb/EmployeeTask/Data/Models/Boss.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeTask.Data.Models
+{
+    public class Boss
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string FullName { get; set; }
+
+        public int TeamId { get; set; }
+
+        public virtual Team Team { get; set; }
+    }
+}
---
+++ EmployeeTaskWeb/EmployeeTask/Data/Models/Team.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeTask.Data.Models
+{
+    public class Team
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int BossId { get; set; }
+
+        public virtual Boss Boss { get; set; }
+
+        public virtual ICollection<Employee> Employees { get; set; }
+    }
+}
